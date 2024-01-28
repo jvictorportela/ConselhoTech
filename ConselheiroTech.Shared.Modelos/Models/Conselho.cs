5d05803c0a55dd83ac8b053756dcd499c98c25d1
jvictorportela <jvictorportela30@outlook.com>
2024-01-28 17:10:04 -0300

    First Version

---
+++ ConselheiroTech.Shared.Modelos/Models/Conselho.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConselhereiroTech.v1.Models;
+
+public class Conselho
+{
+    //Campos do conselho {"slip": { "id": 7, "advice": "Make choices and dont look back."}}
+    [JsonPropertyName("slip")]
+    public Slip SlipConselho { get; set; }
+
+    public class Slip
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("advice")]
+        public string Advice { get; set; }
+    }
+
+    public void ExibirConselho()
+    {
+        Console.WriteLine($"Conselho --> {SlipConselho?.Advice}");
+    }
+}
